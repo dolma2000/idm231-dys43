a4e5ce5edfa618f7b2031c9bf52b9e11a8242c24
Dolma Sherpa <dolma2000@gmail.com>
2020-01-20 22:22:06 -0500

    started html

---
+++ doc/main.js
@@ -0,0 +1,31 @@
+const firstName = 'Dolma';
+const lastName = 'Sherpa';
+const fullName = `${firstName} ${lastName}`;
+
+console.group('My Name Info')
+console.log('firstName:', firstName);
+console.log('lastName:', lastName);
+console.log('fullName:',fullName); 
+console.groupEnd();
+
+//const fullName=firstName + ' ' + lastName; the other "longer" example
+
+const mySwitch = true;
+
+const myArray = ['one', 2, 'three'] ;
+console.log('myArray :', myArray) ;
+console.log(myArray.length);
+
+console.log(myArray[2]);
+
+const colors = ['red','white','blue','green','orange'];
+console.log(colors[2]);
+console.log(colors.length);
+
+const subject = 'idm 231';
+console.log('subject:', subject);
+
+const a=13;
+const b =22;
+const c= a*b;
+console.log(c);
